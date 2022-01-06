ef70990ea9c71f20bfe39b73c696c7207400894c
achmad <achmad.abrar@bridestory.com>
2022-01-06 11:06:04 +0700

    add model

---
+++ app/src/main/java/com/abrar/thecocktailapp_esq/data/models/Drink.kt
@@ -0,0 +1,10 @@
+package com.abrar.thecocktailapp_esq.data.models
+
+data class Drink (
+        val strDrink: String,
+        val strDrinkThumb: String,
+        val idDrink: String,
+        val strImageSource:String,
+        val strImageAttribution: String,
+        val strInstructions: String
+)
